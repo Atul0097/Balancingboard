671ff61828710508b95cdfa3c4cd9331b10c090e
Djordje <db_srb@hotmail.fr>
2018-03-29 03:47:15 +0200

    Communication via bluetooth established
    Now works fine via bluetooth except some COM port connection instability
    The output is now a tuple of the two angle printing every 0.01sec approximately (yet to measure)
    
    .idea file auto added..

--- GameBoard/comunicati.py
+++ GameBoard/comunicati.py
@@ -1,11 +1,59 @@
+import sys
+import glob
+import serial
 
-import inspect
+def serial_ports():
+    """ Lists serial port names
 
-from serial import *
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
-print(inspect.getfile(Serial))
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
-print(dir(Serial))
-ser = Serial.Serial('COM9', 9600, timeout=0,parity=Serial.PARITY_EVEN, rtscts=1)
-#s = ser.read(100)       # read up to one hundred bytes
-#print(s)
+if __name__ == '__main__':
+
+    rep=""
+    ports=serial_ports()
+    print(ports)
+    print()
+    while rep not in ports:
+        rep = input('  Enter a COM port (eg COM1) and press enter:  ')
+
+    ser = serial.Serial(rep, 115200, bytesize = serial.EIGHTBITS , timeout=None, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+    print()
+    port2 = serial_ports()
+    print(port2)
+    print("succesfully connected to: "+rep)
+    print()
+    print("waiting for data:")
+
+    while True:
+        print()
+        s = ser.readline(100)       # read up to one hundred bytes
+        sd = s.decode('latin-1')
+        sd = sd.replace("/nAngle pitch :", "")
+        sd = sd.replace("  Angle roll  :", "")
+        sd = sd.replace(' ', "")
+        sd = sd.replace('\r\n', "")
+        sd2 = sd.split("/")
+        print(sd2)
